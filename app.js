ffd6b30576323072d1f879c67c32528470e40087
Mohmn <naqashmohmin1@gmail.com>
2023-11-10 15:48:25 +0530

    color intensity

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-import fs from 'fs';
+import fs from 'fs/promises';
 import GeoTIFF, { fromUrl, fromUrls, fromArrayBuffer, fromBlob } from 'geotiff';
 import proj4 from 'proj4';
+
+async function saveDataToFile(filePath,dataToSave) {
+	try {
+	  await fs.writeFile(filePath, dataToSave);
+	  console.log('Data saved to file successfully.');
+	} catch (err) {
+	  console.error('Error saving data to file:', err);
+	}
+  }
 
 function toArrayBuffer(buffer) {
 	const arrayBuffer = new ArrayBuffer(buffer.length);
@@ -12,18 +21,35 @@ function toArrayBuffer(buffer) {
 }
 
 async function getTiffData(url) {
-	var buffer = fs.readFileSync('./visualization.tif');
+	var buffer = await fs.readFile('./shade.tif');
 	const arrayBuffer = toArrayBuffer(buffer)
 	const tif = await fromArrayBuffer(arrayBuffer);
 	const img = await tif.getImage();
-	const imgWidth = img.getWidth();
-	const imgHeight = img.getHeight();
-	console.log(img)
+	// const imgWidth = img.getWidth();
+	// const imgHeight = img.getHeight();
+	// await saveDataToFile('vis.txt', img.)
+	// console.log(tif.source)
 	const sourceCrs = `EPSG:3857`;
     const targetCrs = 'EPSG:4326';
 
-	const convertedSW = proj4(sourceCrs, targetCrs, [...img.getOrigin()]);
+	// const sourceCrs = `EPSG:${zoneKey}`;
+    //   const targetCrs = 'EPSG:4326';
 
-	console.log('metadata', convertedSW);
+      const convertedSW = proj4(sourceCrs, targetCrs, [
+        img.getBoundingBox()[0],
+        img.getBoundingBox()[1],
+      ]);
+      const convertedNE = proj4(sourceCrs, targetCrs, [
+        img.getBoundingBox()[2],
+        img.getBoundingBox()[3],
+      ]);
+
+	// const convertedSW = proj4(sourceCrs, targetCrs, [...img.getOrigin()]);
+
+	console.log('metadata', convertedSW,convertedNE);
 }
 getTiffData().then(d => console.log(d));
+
+
+
+
